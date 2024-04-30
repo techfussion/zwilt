f7c60d1ab4bf54aa9d5b184c4bfe0a9da317b2a6
techfussion <muminraj15@gmail.com>
2024-04-30 15:02:46 +0100

    minor change

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,6 +1,6 @@
 import Button from "@/components/button/Button";
 import zwiltArrow from "@/assets/logo-yellow.svg";
-import { expandImg } from "@/utils/images";
+import { menu } from "@/utils/images";
 import './Header.scss';
 import useWindowWidthObserver from "@/hooks/useWindowWidthObserver";
 
@@ -13,21 +13,21 @@ const Header = () => {
             <ul className='navList'>
                 <li className='navItem'><a href="#" className='company'>zwilt<img className='logo' src={zwiltArrow} alt=''/></a></li>
             </ul>
-            {!shouldShowHamburger ?
-                <ul className='navList'>
+            {!shouldShowHamburger &&
+                (<ul className='navList'>
                     <li className='navItem'><a href="#">Find Work</a></li>
                     <li className='navItem'><a href="#">Find Talent</a></li>
                     <li className='navItem'><a href="#">Articles</a></li>
                     <li className='navItem'><a href="#">About Us</a></li>
                     <li className='navItem'><a href="#">Contact Us</a></li>
-                </ul>
-                :
-                <img src={expandImg} alt="" className="hamburger"/>
+                </ul>)
             }
             <ul className='navList'>
                 <li className='navItem'><a href="#">Log In</a></li>
                 <li className='navItem'><Button>Join Now</Button></li>
             </ul>
+
+            {shouldShowHamburger && <img src={menu} alt='' className='hamburger'/>}
         </nav>
     </header>)
 }
--- src/utils/images.js
+++ src/utils/images.js
@@ -36,11 +36,11 @@ import barYellow from '@/assets/bar-yellow.svg';
 import barGreen from '@/assets/bar-green.svg';
 import profileIcon from '@/assets/profile-icon.svg';
 import windowIcon from '@/assets/window-icon.svg';
-import expandImg from '@/assets/images/arrow-down.png';
+import menu from '@/assets/images/menu-bar.png';
 
 
 export {
-    expandImg,
+    menu,
     profileIcon,
     windowIcon,
     barPurple,
