2c02151309c21ff036a0af63ee81e654f4fefe80
techfussion <muminraj15@gmail.com>
2024-04-30 14:34:34 +0100

    minor change of faq section

--- src/layout/Home/FAQ/FAQ.jsx
+++ src/layout/Home/FAQ/FAQ.jsx
@@ -1,38 +1,38 @@
-import './FAQ.scss'
-import arrow from '@/assets/logo-yellow.svg';
-import faqData from '@/data/faq.json';
+import "./FAQ.scss";
+import arrow from "@/assets/logo-yellow.svg";
+import faqData from "@/data/faq.json";
 
 const faqs = faqData.data;
 
 const FAQ = () => {
-    return (
-        <section className="faqSection">
-            <div>
-                 <h2>Frequently asked questions</h2>
-                <div>
-                    {
-                        faqs.map((faq, index) => (
-                            <div key={index} className={`faqGroup ${faq.active ? 'active' : ''}`}>
-                                {
-                                    faq.group && (
-                                        <h4 className={`faqGroupSubHeading ${faq.active ? 'active' : ''}`}>{faq.group}</h4>
-                                    )
-                                }
-                                {
-                                    faq.subGroup && (
-                                        <h4 className='faqGroupSubHeading'>{faq.subGroup}</h4>
-                                    )
-                                }
-                                <p className='faqItem'>{faq.question}</p>
-                                {faq.hasImage && <img src={arrow} alt=''/>}
-                            </div>
-                        ))
-                    }
-                </div>
+  return (
+    <section className="faqSection">
+      <div>
+        <h2>Frequently asked questions</h2>
+        <div>
+          {faqs.map((faq, index) => (
+            <div
+              key={index}
+              className={`faqGroup ${faq.active && !faq.subGroup ? "active" : ""}`}
+            >
+              {faq.group && (
+                <h4
+                  className={`faqGroupSubHeading ${faq.active ? "active" : ""}`}
+                >
+                  {faq.group}
+                </h4>
+              )}
+              {faq.subGroup && (
+                <h4 className="faqGroupSubHeading">{faq.subGroup}</h4>
+              )}
+              <p className="faqItem">{faq.question}</p>
+              {faq.hasImage && <img src={arrow} alt="" />}
             </div>
-           
-        </section>
-    )
-}
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default FAQ;
